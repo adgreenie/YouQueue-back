eccb9d9dcd315ddbc70d6b8fa682a654ed4e87de
Adam Green <adgreenie@gmail.com>
2020-05-14 12:05:35 -0400

    deleting User now deletes the Posts sent from that User

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,65 +1,82 @@
 const User = require("../models/User")
+const Post = require("../models/Post")
 
 const getAllUsers = (req, res) => {
-    User.find({}).then(users => {
-        res.json(users)
-    }).catch(err => {
-        console.log(err)
-        res.send("Error loading users")
+  User.find({})
+    .then((users) => {
+      res.json(users)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.send("Error loading users")
     })
 }
 
 const getUserByUsername = (req, res) => {
-    User.findOne({ username: req.params.username }).then(user => {
-        res.json(user) 
-    }).catch(err => {
-        console.log(err)
-        res.send(`Could not retrieve user "${req.params.username}"`)
+  User.findOne({ username: req.params.username })
+    .then((user) => {
+      res.json(user)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.send(`Could not retrieve user "${req.params.username}"`)
     })
 }
 
 const getUsernameExists = (req, res) => {
-    User.exists({ username: req.params.username }).then(exists => {
-        res.json(exists) 
-    }).catch(err => {
-        console.log(err)
-        res.send(`Error checking if user "${req.params.username}" exists`)
+  User.exists({ username: req.params.username })
+    .then((exists) => {
+      res.json(exists)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.send(`Error checking if user "${req.params.username}" exists`)
     })
 }
 
 const createUser = (req, res) => {
-    User.create(req.body).then(user => {
-        res.json(user)
-    }).catch(err => {
-        console.log(err)
-        res.send(`Error creating user "${req.body.username}"...this username may already be claimed`)
+  User.create(req.body)
+    .then((user) => {
+      res.json(user)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.send(
+        `Error creating user "${req.body.username}"...this username may already be claimed`
+      )
     })
 }
 
 const updateUser = (req, res) => {
-    User.findOneAndUpdate({ username: req.params.username }, req.body)
-        .then(user => {
-            res.json(user)
-    }).catch(err => {
-        console.log(err)
-        res.send("Could not update user...are you trying to change username to one that's already claimed?")
+  User.findOneAndUpdate({ username: req.params.username }, req.body)
+    .then((user) => {
+      res.json(user)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.send(
+        "Could not update user...are you trying to change username to one that's already claimed?"
+      )
     })
 }
 
 const deleteUser = async (req, res) => {
-    User.deleteOne({ username: req.params.username }).then(user => {
-        res.send(`The user "${user.username}" has been deleted`)
-    }).catch(err => {
-        console.log(err)
-        res.send(`Could not delete user with username: "${req.params.username}"`)
+  User.deleteOne({ username: req.params.username })
+    .then((user) => {
+      Post.deleteMany({ sender: user.username })
+      res.send(`The user "${user.username}" has been deleted`)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.send(`Could not delete user with username: "${req.params.username}"`)
     })
 }
 
 module.exports = {
-    getAllUsers,
-    getUserByUsername,
-    getUsernameExists,
-    createUser,
-    updateUser,
-    deleteUser
+  getAllUsers,
+  getUserByUsername,
+  getUsernameExists,
+  createUser,
+  updateUser,
+  deleteUser,
 }
--- db/seed.js
+++ db/seed.js
@@ -1,37 +1,34 @@
-const User = require('../models/User')
-const Post = require('../models/Post')
-const seedData = require('./seedData.json')
-const bcrypt = require('bcrypt')
+const User = require("../models/User")
+const Post = require("../models/Post")
+const seedData = require("./seedData.json")
+const bcrypt = require("bcrypt")
 
 async function seedDB() {
-    try {
-        await User.deleteMany({})
-        await Post.deleteMany({})
+  try {
+    await User.deleteMany({})
+    await Post.deleteMany({})
 
-        seedData.users.forEach(user => {
-            bcrypt.hash(user.password, 10, function(err, hash) {
-                console.log('HASHED PASSWORDS',hash)
-                User.create({
-                    username: user.username,
-                    password: hash,
-                    bio: user.bio,
-                    friends: user.friends
-                })
-            })            
+    seedData.users.forEach((user) => {
+      bcrypt.hash(user.password, 10, function (err, hash) {
+        console.log("HASHED PASSWORDS", hash)
+        User.create({
+          username: user.username,
+          password: hash,
         })
+      })
+    })
 
-        seedData.posts.forEach(post => {
-            Post.create({
-                sender: post.sender,
-                recipient: post.recipient,
-                message: post.message,
-                video: post.video
-            })
-        })
-    }
-    catch (err) {
-        console.log('seed failed', err)
-    }
+    seedData.posts.forEach((post) => {
+      Post.create({
+        sender: post.sender,
+        recipient: post.recipient,
+        message: post.message,
+        video: post.video,
+      })
+    })
+  } catch (err) {
+    console.log("seed failed", err)
+  }
 }
 
 seedDB()
