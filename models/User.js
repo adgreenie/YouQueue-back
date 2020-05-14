06bb0ae3b8c92460b8eaf4df7ca0d9b46cf9dd6a
Adam Green <adgreenie@gmail.com>
2020-05-14 11:49:02 -0400

    removed friends and favorites from User model

--- models/User.js
+++ models/User.js
@@ -1,31 +1,24 @@
-const mongoose = require('../db/connection')
-mongoose.set('useCreateIndex', true)
+const mongoose = require("../db/connection")
+mongoose.set("useCreateIndex", true)
 const Schema = mongoose.Schema
-const uniqueValidator = require('mongoose-unique-validator')
+const uniqueValidator = require("mongoose-unique-validator")
 
 const UserSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        uniqueCaseInsensitive: true
-    },
-    password: { type: String, required: true },
-    image: {
-        type: String,
-        default: 'https://res.cloudinary.com/dnj7porin/image/upload/v1587728631/default-user-icon_ugojoc.png'
-    },
-    bio: String,
-    friends: [String],
-    favorites: [
-        {
-            ref: "Post",
-            type: mongoose.Schema.Types.ObjectId
-        }
-    ]
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    uniqueCaseInsensitive: true,
+  },
+  password: { type: String, required: true },
+  image: {
+    type: String,
+    default:
+      "https://res.cloudinary.com/dnj7porin/image/upload/v1587728631/default-user-icon_ugojoc.png",
+  },
 })
 
 UserSchema.plugin(uniqueValidator)
 
-const User = mongoose.model('User', UserSchema)
+const User = mongoose.model("User", UserSchema)
 module.exports = User
