2c4cb8419423d1409b742d90cb167495d5757974
Adam Green <adgreenie@gmail.com>
2020-05-14 11:10:34 -0400

    ready for heroku deployment

--- models/Post.js
+++ models/Post.js
@@ -1,13 +1,13 @@
-const mongoose = require('../db/connection')
+const mongoose = require("../db/connection")
 const Schema = mongoose.Schema
 
 const PostSchema = new Schema({
-    sender: String,
-    recipient: String,
-    date: { type: String, default: Date() },
-    message: String,
-    video: String
+  sender: String,
+  recipient: String,
+  date: { type: Date, default: Date.now },
+  message: String,
+  video: String,
 })
 
-const Post = mongoose.model('Post', PostSchema)
+const Post = mongoose.model("Post", PostSchema)
 module.exports = Post
